043807180219ff5d597c26f077b67510b815b301
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-10-25 12:01:50 +0200

    route to get all lists

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+/* router.get('/', function (req, res, next) {
   res.json({ 'name': 'Sarmad' })
-});
+}); */
+
+const postController = require('../controllers/postController')
+
+router.get('/', postController.getAllPosts)
 
 module.exports = router;
