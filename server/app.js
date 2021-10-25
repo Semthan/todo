23fb12418c7a3e01e194ab0c3ec97a5c827eb207
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-10-25 12:00:03 +0200

    added mongoose and new route

--- server/app.js
+++ server/app.js
@@ -2,8 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true });
+const { connection } = mongoose;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
 
 var indexRouter = require('./routes/index');
+var postsRouter = require('./routes/posts')
 
 var app = express();
 
@@ -13,5 +25,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+app.use('/', postsRouter)
 
 module.exports = app;
