35962e877aa50da9caf49748b7b8da7bf75e860e
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-01 21:31:53 +0100

    added todos page route

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 import axios from 'axios'
+import { Route, Switch } from 'react-router-dom';
+import { TodosPage } from './Pages/TodosPage';
 
-const url = axios.create({
+/* const url = axios.create({
   baseURL: 'http://localhost:5000/'
-})
+}) */
 
-const getAllTodos = () => url.get('/todo')
+/* const getAllTodos = () => url.get('/todo')
 
 url.get('/todo')
   .then(function (getAllTodos) {
     console.log(getAllTodos.data);
-  })
+  }) */
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Switch>
+        <Route path="/todo" component={TodosPage} />
+      </Switch>
+    </>
   );
 }
 
