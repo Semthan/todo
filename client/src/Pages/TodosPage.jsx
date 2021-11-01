55f50312928a1fa31b9f7ba43cfcdd2934ebb14e
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-01 21:19:36 +0100

    created todos page

---
+++ client/src/Pages/TodosPage.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+import {  getAllTodos } from '../fetches'
+
+export const TodosPage = () => {
+
+
+
+    return (
+        <>
+
+        </>
+    )
+}
