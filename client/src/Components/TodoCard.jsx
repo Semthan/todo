7c461658f6d285107b0ed289426c923d8b19feb0
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-05 20:20:17 +0100

    created todo modal

--- client/src/Components/TodoCard.jsx
+++ client/src/Components/TodoCard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import { Card, Col } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Card, Col, Button, Modal} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export const TodoCard = ({todo}) => {
+
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
     return (
         <Col>
             <Card>
@@ -13,9 +20,28 @@ export const TodoCard = ({todo}) => {
                 </Card.Body>
                 <Card.Footer>
                 <small className="text-muted">Last updated {todo.updatedAt}</small>
+                <Link to={`/todo/${todo._id}`}><Button onClick={handleShow}>Edit</Button></Link>
                 </Card.Footer>
             </Card>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{todo.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {todo.content}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+            </Modal>
         </Col>
+
     )
 }
 
