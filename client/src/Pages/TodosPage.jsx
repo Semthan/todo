218aa314950373dc38e14c6433217096466d2dba
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-06 00:54:43 +0100

    clean up code

--- client/src/Pages/TodosPage.jsx
+++ client/src/Pages/TodosPage.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {  getAllTodos } from '../fetches'
 import { TodoCard } from '../Components/TodoCard'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 
 export const TodosPage = () => {
 
@@ -10,23 +10,10 @@ export const TodosPage = () => {
 
     useEffect(() => {
         getAllTodos().then((res) => setTodos(res.data))
-        
-        
     }, [])
-
-    //console.log(Todos[0].title)
 
     return (
         <Container>
-    {/*             {Todos.map((todo) => { 
-                    return (
-                        <div className="container">
-                            <h3>{todo.title}</h3>
-                            <p>{todo.content}</p> 
-                            <p>{todo.updatedAt}</p> 
-                        </div>
-                    )           
-                })} */}
             <Row xs={1} md={4} className='g-4'>          
                     {!Todos && <p> loading...</p> }
                         {Todos && Object.entries(Todos).map(item => {
