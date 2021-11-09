582601da334b127d04ebd0878efd6a2279772b67
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-09 01:53:17 +0100

    deploy test

--- client/src/fetches.js
+++ client/src/fetches.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const url = axios.create({
+/* const url = axios.create({
     baseURL: process.env.API_KEY || 'http://localhost:5000'
-})
+}) */
+
+const url = axios.create({
+    baseURL: 'https://semthan-todo.herokuapp.com/'
+});
 
 
 export const getAllTodos = () => url.get('/todo')
