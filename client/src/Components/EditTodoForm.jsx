39b8dbf5f996e4a759d4e2e11b2f90950066d98f
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-11-15 15:01:45 +0100

    updated edit

--- client/src/Components/EditTodoForm.jsx
+++ client/src/Components/EditTodoForm.jsx
@@ -1,33 +1,31 @@
-import { useState} from "react"
+import { useState, useEffect} from "react"
 import { useHistory, useParams } from "react-router"
-import { editTodo} from "../fetches"
+import { editTodo, getSingleTodo} from "../fetches"
 
 export const EditTodoForm = () => {
     const { id } = useParams()
 
     let history = useHistory()
 
-    const [formValue,setFormValue ] = useState ({
-        title:"",
-        content: ''
-    })
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        getSingleTodo(id).then((res) => setTodos(res.data))
+    }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const { title, content} = formValue
+        const { title, content} = todos
         const payload = { title, content}
-        console.log(payload)
         
         await editTodo(payload,id)
         
         history.push('/todo')
     }
 
-    const handleChange = (event) => {
-        setFormValue({
-            ...formValue,
-            [event.target.name] : event.target.value
-        })
+    const handleChange = (e) => {
+        let newList = {...todos, [e.target.name]: e.target.value}
+        setTodos(newList);
     }
 
     return (
@@ -36,14 +34,14 @@ export const EditTodoForm = () => {
                 type="text"
                 name="title"
                 placeholder=""
-                value={formValue.title}
+                value={todos?.title}
                 onChange={handleChange}
             />
             <input 
                 type="text"
                 name="content"
                 placeholder=""
-                value={formValue.content}
+                value={todos?.content}
                 onChange={handleChange}
             />            
             <button
--- client/src/fetches.js
+++ client/src/fetches.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 //local
 /* const url = axios.create({
-    baseURL: process.env.API_KEY || 'http://localhost:5000'
-}) */
-
+    baseURL: process.env.API_KEY ||  'http://localhost:3000'
+})
+ */
 axios.defaults.withCredentials = true;
 
 const url = axios.create({
@@ -13,7 +13,7 @@ const url = axios.create({
 
 
 export const getAllTodos = () => url.get('/todo')
-export const getSingleTodo = () => url.get('/todo/:id')
+export const getSingleTodo = (id) => url.get(`/todo/${id}`)
 export const addTodo = (payload) => url.post('/todo/add', payload)
 export const editTodo = (payload, id) => url.post(`/todo/${id}`, payload)
 export const deleteSingleTodo = (id) => url.delete(`/todo/${id}`)
